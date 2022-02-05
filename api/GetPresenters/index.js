1cd3123801ba71256da17a408e2ba130a6568dba
Maziar Adl <maziar.adl@gocious.com>
2022-02-04 17:24:24 -0800

    first function

---
+++ api/GetPresenters/index.js
@@ -0,0 +1,39 @@
+const { BlobServiceClient } = require('@azure/storage-blob');
+
+// A helper function used to read a Node.js readable stream into a string
+async function streamToString(readableStream) {
+  return new Promise((resolve, reject) => {
+    const chunks = [];
+    readableStream.on("data", (data) => {
+      chunks.push(data.toString());
+    });
+    readableStream.on("end", () => {
+      resolve(chunks.join(""));
+    });
+    readableStream.on("error", reject);
+  });
+}
+
+module.exports = async function (context, req) {
+  const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING);
+
+  // Get a reference to a container
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+
+  // blob name
+  const blobName = 'presenters.json';
+
+  // Get a block blob client
+  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+  const downloadBlockBlobResponse = await blockBlobClient.download(0);
+
+  // parse blob contents into string
+  const presenters = await streamToString(downloadBlockBlobResponse.readableStreamBody);
+
+  context.res = {
+    status: 200,
+    body: {
+      presenters,
+    },
+  };
+}
