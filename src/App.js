b17155f5ecb460611c6f9daba1b7dfad4042874a
Franz Salvador <franz.salvador@gocious.com>
2022-03-04 17:29:55 -0800

    booyah

--- src/App.js
+++ src/App.js
@@ -1,39 +1,57 @@
 import './App.css';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import guy from './assets/guy.png';
-import spotlight from './assets/spotlight.png';
+import cn from 'classnames';
 
 const buttonWords = ['Victim', 'Presenter', 'Gocian', 'TED Talker'];
 const buttonWordIndex = Math.floor(Math.random() * buttonWords.length);
-
+const PRESENTATION_STATUS = {
+  NOT_SELECTED: 0,
+  ASSIGNED: 10,
+  PRESENTED: 20,
+}
 function App() {
+  const [presenters, setPresenters] = useState([]);
   useEffect(() => {
     fetch('api/GetPresenters')
       .then(res => {
-        console.log(res);
+        setPresenters(JSON.parse(res.presenters));
       })
       .catch(err => {
         console.log(err);
       })
   }, []);
 
+  const selectPresenter = () => {
+    fetch('api/SelectPresenter')
+      .then(res => {
+        setPresenters(JSON.parse(res.presenters));
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
   return (
     <div className="App">
       <h1 className="header">Pick a Presenter</h1>
-      <button className="pick-button">
+      <button className="pick-button"
+        onClick={selectPresenter}>
         Pick a New {buttonWords[buttonWordIndex]}
       </button>
       <div className="person-wrapper">
         <div className="list-of-people">
-          <div className="person">
-            <img className="person-image" alt="person" src={guy} />
-            <span className="person-name">Adam</span>
-            <img className="spotlight-image" alt="spotlight" src={spotlight} />
-          </div>
-          <div className="person">
-            <img className="person-image" alt="person" src={guy} />
-            <span className="person-name">Bailey</span>
-          </div>
+          {presenters.map(presenter => (
+            <div className="person">
+              <div className={cn('item', {
+                presented: presenter.presentationStatus === PRESENTATION_STATUS.PRESENTED,
+                assigned: presenter.presentationStatus === PRESENTATION_STATUS.ASSIGNED,
+              })}>
+                <img alt="person" src={guy} />
+                <span>{presenter.name}</span>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
